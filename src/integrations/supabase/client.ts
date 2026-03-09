9ad04e6d678ccf766403e37c702ab5b29ced676b
tahleemacademy09 <tahleemacademy09@gmail.com>
2026-03-09 13:43:06 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://wvqeubhupkddtkcdwqcm.supabase.co"
-const supabaseKey = "sb_publishable_cft5dMcRs_6dy0Gzse1HIw_5LrgvnhA"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92Z3NsZWF5YW5uc3hpZmhpcmF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NjY2NzAsImV4cCI6MjA4ODQ0MjY3MH0.LLmRl7Xk72IiAiOs7eA_aQqyt0Cp5w7q5UHmG8rwjLY"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
