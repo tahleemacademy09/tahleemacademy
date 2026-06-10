db6db861dbadadb3628ba86affe506c86b3ad20c
tahleemacademy09 <tahleemacademy09@gmail.com>
2026-06-10 14:00:55 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.fd8e3ad8b8b248d88ffd89189be7cd7b',
+  appId: 'com.tahleemacademy.app',
   appName: 'Tahleem Academy',
   webDir: 'dist',
-  server: {
-    url: 'https://fd8e3ad8-b8b2-48d8-8ffd-89189be7cd7b.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 1500,
